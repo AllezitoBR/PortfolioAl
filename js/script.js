d93fd02ba588a6d9b6bd0bd79082519640674def
AllezitoBR <alle_andrade123@hotmail.com>
2022-08-27 00:30:39 -0300

    color-end

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings:["Desenvolvedor Web Fullstack", "Analista de Dados", "Analista de Produto"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
